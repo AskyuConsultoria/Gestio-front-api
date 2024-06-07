5797c326531a319b6530c60da25059310fa27960
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2024-06-07 19:08:14 -0300

    Fix(login): Agora o login funciona normal

--- public/script.js
+++ public/script.js
@@ -2,10 +2,7 @@ async function login(){
     var user = document.getElementById("usuario").value;
     var senha = document.getElementById("senha").value;
 
-    const data = await fetch(`http://localhost:8080/usuarios/login?usuario=${user}&senha=${senha}`, {
-      method: 'GET',
-      mode: 'no-cors'
-      });
+    const data = await fetch(`http://localhost:8080/usuarios/login?usuario=${user}&senha=${senha}`);
       if (!data.ok) {
         throw new Error('Erro ' + data.statusText);
       }
