aeb03def228959e46399030d6319da4f0f23772b
kayky <kayky.pedroso@sptech.school>
2024-06-12 02:07:53 -0300

    Feat(dashboard): V1 do Front-End e Back-Endfinalizado

---
+++ public/js/graph.js
@@ -0,0 +1,83 @@
+async function buscarViewPedido(usuarioId){
+    try {
+        const resposta = await fetch(`http://localhost:8080/pedido-view/${usuarioId}`, {
+            method: "GET"
+        });
+
+        if (!resposta.ok) {
+            throw new Error(`Erro de servidor, status: ${resposta.status}`);
+        }
+
+        const dados = await resposta.json();
+        console.log(dados);
+        
+        return dados
+
+    } catch (error) {
+        console.log(`Houve um erro: ${error}`);
+    }
+}
+
+async function buscarViewTecido(usuarioId){
+    try {
+        const resposta = await fetch(`http://localhost:8080/tecido-view/${usuarioId}`, {
+            method: "GET"
+        });
+
+        if (!resposta.ok) {
+            throw new Error(`Erro de servidor, status: ${resposta.status}`);
+        }
+
+        const dados = await resposta.json();
+        console.log(dados);
+        
+        return dados
+
+    } catch (error) {
+        console.log(`Houve um erro: ${error}`);
+    }
+}
+
+
+
+function adicionarLabelsPedido(dados){
+    var labelsPedido = []
+    for(var i = 0; i < dados.length; i++){
+        labelsPedido.push(dados[i].nome)
+    }
+    return labelsPedido
+}
+
+function adicionarDadosPedido(dados){
+    var dataPedido = []
+    for(var i = 0; i < dados.length; i++){
+        dataPedido.push(dados[i].qtdPedidos)
+    }
+    return dataPedido
+}
+
+function adicionarLabelsTecido(dados){
+    var labelsTecido = []
+    for(var i = 0; i < dados.length; i++){
+        labelsTecido.push(dados[i].nome)
+    }
+    return labelsTecido
+}
+
+function adicionarDadosTecido(dados){
+    var dataTecido = []
+    for(var i = 0; i < dados.length; i++){
+        dataTecido.push(dados[i].qtdTecidos)
+    }
+    return dataTecido
+}
+
+
+
+
+
+
+
+
+
+
