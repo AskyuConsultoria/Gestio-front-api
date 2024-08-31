a0d5706c12717962dc1b98cdc0bcc1e7dd2d5ae2
GaBriellaCaRdosoInacio <gabriella.inacio@sptech.school>
2024-08-31 01:23:57 -0300

    Fix(modal): correção de logica de exibição

--- public/js/etapa.js
+++ public/js/etapa.js
@@ -1,145 +1,201 @@
-async function buscarFinalizado(usuarioId, etapaId) {
+var cardsCriados = false;
+
+async function buscarAtivo(usuarioId, etapaId) {
   try {
     const response = await fetch(`http://localhost:8080/agendamento/etapa-ativo/${usuarioId}/${etapaId}`, {
+      method: "GET"
+    });
+    const data = await response.json();
+    console.log("Resposta: ", data);
+
+    return data;
+  } catch (error) {
+    console.error("Erro: ", error);
+    return [];
+  }
+}
+
+async function buscarInativo(usuarioId, etapaId) {
+  try {
+    const response = await fetch(` http://localhost:8080/agendamento/etapa-inativo/${usuarioId}/${etapaId}`, {
       method: "GET"
     }
     );
+    console.log(response)
     const data = await response.json();
+    // validação de se ta vazio ou não, aqui ou no back
     console.log("Resposta: ", data);
 
     return data
-
   } catch (error) {
     return console.error("Erro: ", error);
+
   }
 }
 
-async function criarModalAtivo(usuarioId, etapaId, nomeEtapa) {
-  try {
-    const response = await fetch(`http://localhost:8080/agendamento/etapa-ativo/${usuarioId}/${etapaId}`, {
-      method: "GET"
+async function criarModal(usuarioId, etapaId, nomeEtapa) {
+  const nomeUpper = nomeEtapa.toUpperCase();
+
+  const cardsAtivos = await buscarAtivo(usuarioId, etapaId);
+  const totalOriginal = cardsAtivos.length;
+
+  var cardPessoa = cardsAtivos.map(agenda => {
+    return `<div class="card-pessoa d-flex align-items-center justify-content-evenly flex-column mt-3">
+          <div class="texto-card">
+            <div class="nome-pessoa">${agenda.cliente.nome} ${agenda.cliente.sobrenome}</div>
+            <span class="dthora">${formatarData(agenda.dataInicio)} ${formatarHorario(agenda.dataInicio)} - ${formatarHorario(agenda.dataFim)}</span> 
+          </div>
+        </div>`;
+  }).join('');
+
+  const cardsConcluidos = cardsAtivos.map(agenda => {
+    return `<div class="card-pessoa finalizados d-flex align-items-center justify-content-evenly flex-column mt-3">
+          <div class="texto-card">
+            <div class="nome-pessoa">${agenda.cliente.nome} ${agenda.cliente.sobrenome}</div>
+            <span class="dthora">${formatarData(agenda.dataInicio)} - ${formatarHorario(agenda.dataFim)}</span> 
+          </div>
+        </div>`;
+  }).join('');
+
+  if (cardsAtivos.some(agenda => agenda.etapa.ultimaEtapa === true)) {
+    var modal = document.querySelector('.finalizado');
+
+    document.getElementById(etapaId).addEventListener('click', function () {
+      if (!modal.classList.contains('subir')) {
+        modal.classList.remove('descer');
+        modal.classList.add('subir');
+        console.log("modal subindo");
+      }
     });
-    const data = await response.json();
-    console.log("Resposta: ", data);
 
-    // Remove qualquer offcanvas anterior
-    const offcanvasAnterior = document.getElementById('offcanvasBottom');
-    if (offcanvasAnterior) {
-      offcanvasAnterior.remove();
-    }
+    document.getElementById('fechar').addEventListener('click', function () {
+      if (!modal.classList.contains('descer')) {
+        modal.classList.remove('subir');
+        modal.classList.add('descer');
+        console.log("modal descendo");
+      }
+    });
+    criarFinalizado(nomeUpper, nomeEtapa, cardsConcluidos);
+  }
 
-    // Cria o offcanvas
-    const container = document.getElementById('container');
-    const offcanvasDiv = document.createElement('div');
-    offcanvasDiv.className = 'offcanvas offcanvas-bottom h-100';
-    offcanvasDiv.tabIndex = -1;
-    offcanvasDiv.id = 'offcanvasBottom';
-    offcanvasDiv.ariaLabelledby = 'offcanvasBottomLabel';
 
-    // Nome da etapa com uppercase
-    const nomeUpper = nomeEtapa.toUpperCase();
+  else {
+    var modal = document.querySelector('.comum');
 
-    // Cria um elemento que vai receber o conteúdo do offcanvas
-    const offcanvasContent = document.createElement('div');
+    document.getElementById(etapaId).addEventListener('click', function () {
+      if (!modal.classList.contains('subir')) {
+        modal.classList.remove('descer');
+        modal.classList.add('subir');
+        console.log("modal subindo");
+      }
+    });
 
-    // Busca os pedidos finalizados
-    var finalizados = await buscarFinalizado(usuarioId, etapaId);
-    const totalOriginal = data.length;
+    document.getElementById('fechar2').addEventListener('click', function () {
+      if (!modal.classList.contains('descer')) {
+        modal.classList.remove('subir');
+        modal.classList.add('descer');
+        console.log("modal descendo");
+      }
+    });
+    criarComum(etapaId, nomeUpper, nomeEtapa, cardPessoa, totalOriginal, usuarioId);
+  }
+}
 
-    // Mapeia os cards de pessoas com pedidos ativos
-    var cardPessoa = data.map(agenda => {
-      return `<div class="card-pessoa d-flex align-items-start justify-content-evenly flex-column mt-3">
-         <div class="texto-card">
-           <div class="nome-pessoa">${agenda.cliente.nome} ${agenda.cliente.sobrenome}</div>
-           <span class="dthora">${formatarData(agenda.dataInicio)} ${formatarHorario(agenda.dataInicio)} - ${formatarHorario(agenda.dataFim)}</span> 
-         </div>
-       </div>`;
-    }).join('');
+function criarFinalizado(nomeUpper, nomeEtapa, cardsConcluidos) {
+  if (!cardsCriados) {
 
-    // Mapeia os cards de pessoas com pedidos concluídos
-    const cardsConcluidos = finalizados.map(agenda => {
-      return `<div class="card-pessoa finalizado d-flex align-items-start justify-content-evenly flex-column mt-3">
-        <div class="texto-card">
-          <div class="nome-pessoa">${agenda.cliente.nome} ${agenda.cliente.sobrenome}</div>
-          <span class="dthora">${formatarData(agenda.dataInicio)} - ${formatarHorario(agenda.dataFim)}</span> 
-        </div>
-      </div>`;
-    }).join('');
+    var tituloCard = document.querySelector('.modal-etapa-title');
+    tituloCard.innerHTML = nomeUpper;
 
-    // Define o conteúdo do offcanvas
-    if (finalizados.some(agenda => agenda.etapa.ultimaEtapa === true)) {
-      offcanvasContent.innerHTML = `
-         <div class="offcanvas-header d-flex align-items-center">
-          <div class="header-off d-flex align-items-center justify-content-evenly flex-row">
-            <h5 class="offcanvas-title mt-2" id="offcanvasTopLabel">${nomeUpper}</h5>
-          </div>
-          <img src="./assets/marca-x 2.svg" alt="voltar" type="button" data-bs-dismiss="offcanvas" id="fechar">
-        </div>
-        <div class="offcanvas-body">
-          <div class="etapa-title mt-3">
-            <span class="pedido-etapa">PEDIDOS NA ETAPA <span class="nome-etapa">${nomeUpper}</span></span>
-          </div>
-          <div class="container-pessoa">
-            ${cardsConcluidos}
-          </div>
-        </div>
-      `;
+    var nomeEtapa = document.querySelector('.nome-etapa');
+    nomeEtapa.innerHTML = nomeUpper;
+
+    var containerPessoa = document.querySelector('#container-pessoa-finalizado');
+    containerPessoa.innerHTML = cardsConcluidos;
+
+    cardsCriados = true;
+  }
+}
+
+async function criarComum(etapaId, nomeUpper, nomeEtapa, cardPessoa, totalOriginal, usuarioId) {
+  cardsCriados = false;
+
+  console.log("Criando cards comuns...");
+
+  var tituloCard = document.querySelector('.title2');
+  tituloCard.innerHTML = nomeUpper;
+
+  var nomeEtapa = document.querySelector('.nome2');
+  nomeEtapa.innerHTML = nomeUpper;
+
+  var containerPessoa = document.querySelector('#container-pessoa-comum');
+  containerPessoa.innerHTML += cardPessoa
+  containerPessoa.style.marginRight='35%'
+
+
+  var totalPedidos = document.querySelector('.total-pedidos');
+  totalPedidos.innerHTML = totalOriginal;
+
+  const switchElement = document.querySelector('.custom-switch');
+  var isActive = true; // Variável para controlar o estado do switch
+
+  switchElement.addEventListener('click', async function () {
+    const etapa = document.querySelector('.status-etapa');
+    const subPedido = document.querySelector('.sub-pedido');
+    const totalPedidos = document.querySelector('.total-pedidos');
+
+    if (isActive) {
+      // Se o switch está ativo, busca os cancelados
+      const cancelados = await buscarInativo(usuarioId, etapaId);
+
+      if (cancelados && cancelados.length > 0) {
+        containerPessoa.innerHTML=''
+        containerPessoa.innerHTML = cancelados.map(cancelado => {
+          return `<div class="card-pessoa cancelado d-flex align-items-start justify-content-evenly flex-column mt-3">
+                        <div class="texto-card">
+                          <div class="nome-pessoa">${cancelado.cliente.nome} ${cancelado.cliente.sobrenome}</div>
+                          <span class="dthora">${formatarData(cancelado.dataInicio)} - ${formatarHorario(cancelado.dataFim)}</span> 
+                        </div>
+                      </div>`;
+        }).join('');
+        totalPedidos.innerHTML = cancelados.length;
+      } else {
+        totalPedidos.innerHTML = '0';
+        containerPessoa.innerHTML = `<img src="./assets/not-found.svg">
+                    <div>Você não possui pedidos cancelados</div>`;
+        containerPessoa.classList.add('zero');
+        etapa.innerHTML = '';
+      }
+
+      subPedido.style.marginLeft = '8%';
+      containerPessoa.style.marginLeft = '12%';
+      etapa.style.marginRight = '15%';
+      etapa.innerHTML = '<div style="color: red">CANCELADOS</div>'; 
+
     } else {
-      offcanvasContent.innerHTML = `
-        <div class="offcanvas-header d-flex align-items-center">
-          <div class="header-off d-flex align-items-center justify-content-evenly flex-row">
-            <h5 class="offcanvas-title mt-2" id="offcanvasTopLabel">${nomeUpper}</h5>
-          </div>
-          <img src="./assets/marca-x 2.svg" alt="voltar" type="button" data-bs-dismiss="offcanvas" id="fechar">
-        </div>
-        <div class="offcanvas-body">
-          <div class="etapa-title mt-3">
-            <span class="pedido-etapa">PEDIDOS NA ETAPA <span class="nome-etapa">${nomeUpper}</span></span>
-          </div>
-          <div class="sub-pedido mt-3 d-flex align-items-center justify-content-evenly flex-row mt-3">
-            <div>VOCÊ ESTÁ VENDO PEDIDOS</div>
-            <div class="status-etapa">EM ANDAMENTO</div>
-          </div>
-          <div class="barra-busca d-flex align-items-center justify-content-evenly mt-3">
-            <img src="./assets/search.svg" alt="lupa-pesquisa">
-            <input type="text" placeholder="Pesquise os clientes do pedido">
-          </div>
-          <div class="switch-card d-flex align-items-center flex-column">
-            <div class="form-check form-switch d-flex align-items-center justify-content-between mt-3 p-0 m-0">
-              <div class="text-switch mt-1">Total de pedidos: <span class="total-pedidos">${data.length}</span></div>
-              <input class="form-check-input custom-switch" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-            </div>
-            <div class="container-pessoa">
-              ${cardPessoa}
-            </div>
-          </div>
-        </div>
-      `;
+      // Se o switch não está ativo, mostra os pedidos em andamento
+      containerPessoa.innerHTML=''
+      etapa.innerHTML = 'EM ANDAMENTO';
+      totalPedidos.innerHTML = totalOriginal;
+      containerPessoa.innerHTML = cardPessoa
+      containerPessoa.classList.remove('zero');
+      etapa.style.marginRight = '9%';
+      subPedido.style.marginLeft = '6%';  
+
     }
 
-    // Coloca o conteúdo do offcanvas dentro do container
-    offcanvasDiv.appendChild(offcanvasContent);
-    container.appendChild(offcanvasDiv);
+    // Alterna o estado do switch
+    isActive = !isActive; // Inverte o estado
 
-    // Inicializa o offcanvas do Bootstrap
-    const offcanvas = new bootstrap.Offcanvas(offcanvasDiv);
+    // Opcional: Atualize a aparência do switch para refletir o estado
+    if (isActive) {
+      switchElement.classList.remove('active'); // Remova a classe que indica que está ativo
+    } else {
+      switchElement.classList.add('active'); // Adicione a classe que indica que está inativo
+    }
+  });
 
-    // Adiciona o evento de fechamento
-    const fecharOffcanvas = document.getElementById('fechar');
-    fecharOffcanvas.addEventListener('click', () => {
-      offcanvas.hide();
-    });
-
-    // Exibe o modal
-    offcanvas.show();
-
-    return offcanvas;
-
-  } catch (error) {
-    console.error("Erro: ", error);
-  }
 }
-
 
 
 function formatarData(data) {
@@ -175,22 +231,6 @@ function formatarHorario(data) {
   return horarioFormatado
 }
 
-async function buscarInativo(usuarioId, etapaId) {
-  try {
-    const response = await fetch(` http://localhost:8080/agendamento/etapa-inativo/${usuarioId}/${etapaId}`, {
-      method: "GET"
-    }
-    );
-    const data = await response.json();
-    console.log("Resposta: ", data);
-
-    return data
-  } catch (error) {
-    return console.error("Erro: ", error);
-
-  }
-}
-
 async function buscarTotalEtapa() {
   const usuarioId = sessionStorage.getItem("id")
   const response = await fetch(`http://localhost:8080/agendamento-view-total-etapa/${usuarioId}`, {
@@ -218,23 +258,22 @@ async function listarEtapas() {
 
       if (i < totalEtapa.length && data[i].ativo != false) {
         cardAgendamento.innerHTML += `
-        <div class="etapa-card card mx-auto position-relative mb-3" value="${nomeEtapa}" id="${etapaId}" data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasBottom">
-        <div class="row g-0">
-          <div class="col-md-8">
-            <div class="card-body">
-              <div class="d-flex flex-row align-items-center justify-content-between mx-3">
-                <h5 class="etapa-nome card-title mb-0">${nomeEtapa}</h5>
-                <div class="total-etapa d-flex flex-column align-items-center">
-                <span id="n-total">${totalEtapa[i].qtd_agendamento}</span>
-                  <span>total</span>
+          <div class="etapa-card card mx-auto position-relative mb-3" value="${nomeEtapa}" id="${etapaId}">
+          <div class="row g-0">
+            <div class="col-md-8">
+              <div class="card-body">
+                <div class="d-flex flex-row align-items-center justify-content-between mx-3">
+                  <h5 class="etapa-nome card-title mb-0">${nomeEtapa}</h5>
+                  <div class="total-etapa d-flex flex-column align-items-center">
+                  <span id="n-total">${totalEtapa[i].qtd_agendamento}</span>
+                    <span>total</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      `;
+        `;
       }
     }
 
@@ -249,9 +288,8 @@ async function listarEtapas() {
       const nomeEtapa = card.getAttribute('value');
       const etapaId = card.getAttribute('id');
 
-      var offcanvas = await criarModalAtivo(usuarioId, etapaId, nomeEtapa);
+      await criarModal(usuarioId, etapaId, nomeEtapa);
 
-      offcanvas.show()
 
     });
   });
